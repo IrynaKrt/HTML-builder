d32505aff5342cdea1cbbc6567af592cb7dac2be
IrynaKrt <rinakashi13@mail.ru>
2023-05-04 15:19:59 +0300

    fix: update the solution with 'path'

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
+const path = require('path');
 
-const stream = fs.createReadStream('./01-read-file/text.txt', 'utf-8');
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 stream.on('data', (item) => console.log(item));
 stream.on('end', () => console.log('End!'));
 stream.on('error', (error) => console.log('Error', error.message));
