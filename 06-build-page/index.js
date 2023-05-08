95055d9524a1e2c22a8fabf431a4aeb3cca99a1f
IrynaKrt <rinakashi13@mail.ru>
2023-05-09 01:04:38 +0300

    fix: debug the functions at 06-solution

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,15 +1,12 @@
 const { stdout, exit } = process;
 
 const fs = require('fs');
-const fsPromise = require('fs/promises');
+const promise = require('fs/promises');
 const path = require('path');
-
-// HTML
 
 const getData = async(name) => {
   try {
-    const data = await fsPromises.readFile(path.join(__dirname, '/components', name), { encoding: 'utf8' });
-
+    const data = await promise.readFile(path.join(__dirname, '/components', name), { encoding: 'utf8' });
     return data;
   } catch (error) {
     stdout.write('Error');
@@ -17,25 +14,24 @@ const getData = async(name) => {
   }
 }
 
-const replaceData = async(elementsArr) => {
+const replaceItems = async(elem) => {
   let read = fs.createReadStream(path.resolve(__dirname, 'template.html'), 'utf-8');
 
   function streamToString (stream) {
     const chunks = [];
     return new Promise((resolve, reject) => {
       stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
-      stream.on('error', (err) => reject(err));
+      stream.on('error', (error) => reject(error));
       stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
     });
   }
 
   let content = await streamToString(read);
 
-  for (const element of elementsArr) {
+  for (const element of elem) {
     if (content.includes(`{{${element}}}`)) {
-      let elementItem = await getData(`${element}.html`);
-
-      content = await content.replaceAll(`{{${element}}}`, elementItem);
+      let elementData = await getData(`${element}.html`);
+      content = await content.replaceAll(`{{${element}}}`, elementData);
     }
   }
 
@@ -43,16 +39,16 @@ const replaceData = async(elementsArr) => {
   return content;
 }
 
-const readFiles = async(dir) => {
+const readAllFiles = async(dir) => {
   try {
-    const arrayOfFiles = [];
-    const files = await fsPromises.readdir(dir, {withFileTypes: true});
+    const arr = [];
+    const files = await promise.readdir(dir, {withFileTypes: true});
     for (const file of files)
       if (!file.isDirectory() && path.extname(file.name) === '.html') {
         let name = path.parse(file.name).name;
-        arrayOfFiles.push(name);
+        arr.push(name);
       }
-    return arrayOfFiles;
+    return arr;
   } catch (error) {
     stdout.write('Error');
     exit();
@@ -60,76 +56,83 @@ const readFiles = async(dir) => {
 }
 
 const createHTML = async() => {
-  let elements = [];
-  elements = await readFiles(path.join(__dirname, 'components'));
+  let arr = [];
+  arr = await readAllFiles(path.join(__dirname, 'components'));
 
-  const createdHTML = await replaceData(elements);
-  await fsPromise.writeFile(path.join(__dirname, '/project-dist', 'index.html'), createdHTML);
+  const html = await replaceItems(arr);
+  await promise.writeFile(path.join(__dirname, '/project-dist', 'index.html'), html);
 }
 
-// CSS
 
-const readCssContent = async(dir, file) => {
+const readCSSFolder = async(dir, file) => {
   try {
-    const filePath = path.join(dir, file);
-    const data = await fsPromises.readFile(filePath, { encoding: 'utf8' });
+    const pathOfItem = path.join(dir, file);
+    const data = await promise.readFile(pathOfItem, { encoding: 'utf8' });
+
     return data;
-  } catch (err) {
-    console.log(err);
+  } catch (error) {
+    stdout.write('Error');
+    exit();
   }
 }
 
 const readStyles = async(dir) => {
-    try {
-      await fs.writeFile(path.join(__dirname, '/project-dist', 'bundle.css'), '');
+  try {
+    await promise.writeFile(path.join(__dirname, '/project-dist', 'style.css'), '');
 
-      const files = await fs.readdir(dir, {withFileTypes: true});
+    const styles = await promise.readdir(dir, {withFileTypes: true});
 
-      for (const file of files)
-        if (!file.isDirectory() && path.extname(file.name) === '.css') {
-          const content = await readFolder(dir, file.name);
-          await fs.appendFile(path.join(__dirname, '/project-dist', 'bundle.css'), content);
-        }
-    } catch (error) {
-      stdout.write('there was an error:', error.message);
-    }
+    for (const file of styles)
+      if (!file.isDirectory() && path.extname(file.name) === '.css') {
+        const content = await readCSSFolder(dir, file.name);
+
+        await promise.appendFile(path.join(__dirname, '/project-dist', 'style.css'), content);
+      }
+  } catch (error) {
+    stdout.write('Error');
+    exit();
   }
+}
 
-const createCss = async() => {
-  await fsPromise.rm(path.join(__dirname, '/project-dist', 'style.css'), { recursive: true, force: true });
+const createCSS = async() => {
+  await promise.rm(path.join(__dirname, '/project-dist', 'style.css'), { recursive: true, force: true });
   await readStyles(path.join(__dirname, 'styles'));
 }
 
 // Assets
 
-const copyDir = async (src,dest) => {
-    const copy = await fs.readdir(src, {withFileTypes: true});
+const copyDir = async(src, dist) => {
+  const entries = await promise.readdir(src, {withFileTypes: true});
+  await promise.mkdir(dist, {recursive: true});
 
-    await fs.mkdir(dest, {recursive: true});
-
-    for(let item of copy) {
-      const init = path.join(src, item.name);
-      const output = path.join(dest, item.name);
-
-      if(item.isDirectory()) {
-        await copyDir(init, output);
-      } else {
-        await fs.copyFile(init, output);
-      }
+  for(let entry of entries) {
+    const srcPath = path.join(src, entry.name);
+    const distPath = path.join(dist, entry.name);
+    if(entry.isDirectory()) {
+      await copyDir(srcPath, distPath);
+    } else {
+      await promise.copyFile(srcPath, distPath);
     }
   }
+}
 
-const makeAssets = async() => {
-  await fsPromise.rm(path.join(__dirname,'/project-dist', 'assets'), { recursive: true, force: true });
+const createAssets = async() => {
+  await promise.rm(path.join(__dirname,'/project-dist', 'assets'), { recursive: true, force: true });
   await copyDir(path.join(__dirname, 'assets'), path.join(__dirname, '/project-dist', 'assets'));
 }
 
 // Full
 
-const makeBundle = async() => {
-  await makeAssets();
-  await createHTML();
-  await createCss();
+const bundle = async() => {
+  try {
+    await createAssets();
+    await createHTML();
+    await createCSS();
+    stdout.write('Done!');
+  } catch (error) {
+    stdout.write('Error');
+    exit();
+  }
 }
 
-makeBundle();
+bundle();
