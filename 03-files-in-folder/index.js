8492a2fbb047246cc199d488eb63fb51b7893da5
IrynaKrt <rinakashi13@mail.ru>
2023-05-08 15:31:20 +0300

    feat: add the 03-solution

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -13,8 +13,8 @@ fs.readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true}, (err, f
         if (file.isFile()) {
         fs.stat(path.join(__dirname, 'secret-folder', file.name), (err, stat) => {
             if (err) {
-            stdout.write('Error');
-            exit();
+                stdout.write('Error');
+                exit();
             }
             stdout.write(`${file.name.split('.')[0]} - ${path.extname(file.name).slice(1)} - ${(stat.size / 1024).toFixed(3)}kb `);
         })
