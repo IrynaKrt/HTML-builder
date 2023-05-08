2428b65e313dcac7733f0ff4b223881306ccde44
IrynaKrt <rinakashi13@mail.ru>
2023-05-08 15:30:25 +0300

    feat: add the 03-solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const { stdout, exit } = process;
+
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true}, (err, files) => {
+    if (err) {
+        stdout.write('Error');
+        exit();
+    }
+
+    files.forEach(file => {
+        if (file.isFile()) {
+        fs.stat(path.join(__dirname, 'secret-folder', file.name), (err, stat) => {
+            if (err) {
+            stdout.write('Error');
+            exit();
+            }
+            stdout.write(`${file.name.split('.')[0]} - ${path.extname(file.name).slice(1)} - ${(stat.size / 1024).toFixed(3)}kb `);
+        })
+        }
+    })
+})
