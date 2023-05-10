80740649a1a7a88462d500d99fe3b2c0187eef25
IrynaKrt <rinakashi13@mail.ru>
2023-05-10 15:19:22 +0300

    fix: debug the functions at 05-solution

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -6,9 +6,10 @@ const path = require('path');
 const readFolder = async(dir, file) => {
   try {
     const filePath = path.join(dir, file);
-    const data = await fs.readFile(filePath, 'utf8');
+    const data = await fs.readFile(filePath, { encoding: 'utf8' });
 
     return data;
+
   } catch (error) {
     stdout.write('Error');
     exit();
@@ -18,21 +19,21 @@ const readFolder = async(dir, file) => {
 const readStyles = async(dir) => {
   try {
     await fs.writeFile(path.join(__dirname, '/project-dist', 'bundle.css'), '');
-
     const files = await fs.readdir(dir, {withFileTypes: true});
 
-    for (const file of files)
+    for (const file of files){
       if (!file.isDirectory() && path.extname(file.name) === '.css') {
         const content = await readFolder(dir, file.name);
         await fs.appendFile(path.join(__dirname, '/project-dist', 'bundle.css'), content);
       }
+    }
   } catch (error) {
     stdout.write('there was an error:', error.message);
   }
 }
 
 const updateBundle = async() => {
-  await fs.rm(path.join(__dirname, '/project-dist', 'bundle.css'));
+  await fs.rm(path.join(__dirname, '/project-dist', 'bundle.css'), { recursive: true, force: true });
   await readStyles(path.join(__dirname, 'styles'));
 }
 
